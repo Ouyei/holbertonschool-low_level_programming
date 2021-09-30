fb6a4f7fb25fb946b91203406b69992636b552a8
Ouyei <obedat@me.com>
2021-09-30 12:56:02 -0700

    unix_files

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a text
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+	printf("with proper grammar, bit the outcome is a oiece of art,\n");
+	return (0);
+}
