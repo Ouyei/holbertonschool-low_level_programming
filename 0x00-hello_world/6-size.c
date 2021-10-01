24293681261f86680c99c74e578d3d07fe3e9e39
Ouyei <obedat@me.com>
2021-09-30 17:37:01 -0700

    unix_files

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of many var types on the computer it is compiled and run on.
+ *
+ * Return: Always 0 (succes)
+ */
+int main(avoid)
+{
+	printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+	printf("Size of an int; %zu bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float);
+
+	return (0);
+}
