bdbf720e532790c65fadf3022832ac79bc500400
Ouyei <obedat@me.com>
2021-10-01 06:48:36 -0700

    low_level_programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include<stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
+ *
+ * Return: Allways 0 (Success)
+ * */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
