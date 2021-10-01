adb1ea27a84b572e84a8f4a4cbe3eed57a6e3e6d
Ouyei <obedat@me.com>
2021-10-01 07:15:23 -0700

    low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,19 +1,16 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <unistd.h>
 
 /**
- *main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by new line, to standard error.
  *
- * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
- *
- * Return: Allways 0 (Success)
- * 
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2, 
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		59);
+	write(2,
+			"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59);
 
-	return (1);
+	return(1);
 }
