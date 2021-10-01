1e72120fb19a973da0f8434d3a27090c0967f3d7
Ouyei <obedat@me.com>
2021-10-01 11:37:03 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Allways 0 (Success)
+ */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+		putchar('\n');
+
+return (0);
+}
