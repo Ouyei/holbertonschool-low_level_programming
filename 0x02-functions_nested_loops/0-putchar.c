aba0170c69f598dd1bef1e1ecc306d025d738b4e
Ouyei <obedat@me.com>
2021-10-05 06:39:55 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,15 @@
  */
 int main(void)
 {
-	_putchar('P');
-	_putchar('U');
-	_putchar('T');
-	_putchar('C');
-	_putchar('H');
-	_putchar('A');
-	_putchar('R');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
