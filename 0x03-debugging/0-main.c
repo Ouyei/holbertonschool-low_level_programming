05cfd6aea3c3364165875636aea6070bd9a6edc6
Ouyei <obedat@me.com>
2021-10-06 10:38:55 -0700

    fixed_files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function that prints if integer is positve or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
