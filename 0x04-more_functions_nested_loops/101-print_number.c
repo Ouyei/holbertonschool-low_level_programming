fe062712f0b84b067051d564e9fbd99ad608d4ed
Ouyei <obedat@me.com>
2021-10-13 09:49:31 -0700

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints # using _putchar function
+ * @n: the integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int num = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+	_putchar((num % 10) + '0');
+}
