0fa7845ca870f67e650e6e36793698dc13552ca7
Ouyei <obedat@me.com>
2021-10-15 09:56:49 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - converts strings to uppercase
+ * @s: string 2 convert
+ *
+ * Return: char pointer
+ */
+char *string_toupper(char *n)
+{
+	int i;
+
+	i = 0;
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 'a' && n[i] <= 'z')
+			n[i] = n[i] -32;
+		i++;
+	}
+	return (n);
+}
