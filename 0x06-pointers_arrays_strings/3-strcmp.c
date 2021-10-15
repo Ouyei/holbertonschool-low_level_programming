e4bc529336e0e4b2905b1bc300a7a16bf17b8dee
Ouyei <obedat@me.com>
2021-10-15 09:14:22 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- * performs lexicogarphic comparison of two string.
+ * _strcmp - performs lexicogarphic comparison of two string.
  * @s1: the first string
  * @s2: the second string
  * Return: *s1 - *s2
  */
 
-int _strcmp(char *s1, char *s2);
-
+int _strcmp(char *s1, char *s2)
 {
-	char *s1 = s1;
-	char *s2 = s2; /*Decalare poniter*/
 
 	/*WHILE*/
 	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
