6f209ed47053a8ab21289974de92562348dca0fd
Ouyei <obedat@me.com>
2021-10-22 18:26:35 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,39 +11,19 @@
  */
 int main(int argc, char *argv[])
 {
-	int quant, coins;
+	int i, n, j = 0, coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	quant = atoi(argv[1]);
-	coins = 0;
-	if (quant > 25)
-	{
-		while (quant >= 25)
-			quant -= 25, coins++;
-	}
-	if (quant > 10 && quant < 25)
-	{
-		while (quant >= 10)
-			quant -= 10, coins++;
-	}
-	if (quant > 5 && quant < 10)
-	{
-		while (quant >= 5)
-			quant -= 5, coins++;
-	}
-	if (quant < 2 && quant < 5)
-	{
-		while (quant >= 2)
-			quant -= 2, coins++;
-	}
-	if (quant == 1 || quant == 2 || quant == 5 || quant == 10 || quant == 25)
-	{
-		coins++;
-	}
-	printf("%d\n", coins);
+		return (printf("Error\n"), 1);
+	n = atoi(argv[1]);
+	if (n < 0)
+		return (puts("0"), 1);
+	for (i = 0; i < 5; i++)
+		if (n / coins[i])
+		{
+			j += n / coins[i];
+			n %= coins[i];
+		}
+	printf("%d\n", j);
 	return (0);
 }
