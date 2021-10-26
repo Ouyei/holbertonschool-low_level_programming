85022c5fa2253862aebe9197a07121946c3444ec
Ouyei <obedat@me.com>
2021-10-26 12:29:38 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,32 +9,21 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2;
-	char *str;
+	int len1 = 0, len2 = 0;
+	char *conc;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	len1 = len2 = 0;
-	while (s1[len1] != '\0')
+	while (s1 && *s1++)
 		len1++;
-	while (s2[len2] != '\0')
+	while (s2 && *s2++)
 		len2++;
-	str = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (str == NULL)
+	conc = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!conc)
 		return (NULL);
-	len1 = len2 = 0;
-	while (s1[len2] != '\0')
-	{
-		str[len1] = s1[len1];
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		str[len1] = s2[len2];
-		len1++, len2++;
-	}
-	str[len1] = '\0';
-	return (str);
+	conc += len1 + len2;
+	*conc = '\0'; /**Invalid pointer correction**/
+	for (s2--; len2--;)
+		*--conc = *--s2;
+	for (s1--; len1--;)
+		*conc = *--s1;
+	return (conc);
 }
