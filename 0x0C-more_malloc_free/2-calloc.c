200a18dcb7dfac882235887b8b55cc91d89a4483
Ouyei <obedat@me.com>
2021-10-28 10:31:04 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocat memory for an array init to zero.
+ * @nmemb: Number of elements.
+ * @size: Byte size of each element.
+ * Return: Void pointer to array space.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (!nmemb ||!size)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (!p)
+		return(NULL);
+
+	for (i =0; i < nmemb * size; i++)
+		p[i] = 0;
+
+	return (p);
+}
