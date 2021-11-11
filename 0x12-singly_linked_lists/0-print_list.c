dd566bf38173e27d4f8e4869161612d413f32f49
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-11 09:15:22 -0800

    fixed_files

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,12 +11,12 @@ size_t print_list(const list_t *h)
 {
 	size_t size= 0;
 	
-	while (h)
+	while (h != NULL)
 	{
 		if (!h->str)
 			printf ("[0] (nil\n");
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		size++;
 	}
