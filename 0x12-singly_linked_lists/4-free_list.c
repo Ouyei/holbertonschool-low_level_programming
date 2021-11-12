58e88b35494f511f015dad8ced0bd5cb7036f7a4
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-11 16:44:07 -0800

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees list
+ * @head: list_t to freed
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+
+	free(head);
+}
