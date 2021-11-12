a0a53dd4aea5a07a6edad2aecd7218a0206eb3a7
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-11 16:37:04 -0800

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+
+void first(void) __attribute__ ((constructor));
+
+/**
+  * first - ...
+  *
+  * Return: Nothing.
+  */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
