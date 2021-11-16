b21c6f8ac1c0160ffb52939c4e85bb218abdb319
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-16 09:36:35 -0800

    new_comments

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
-* sum_listint - sum the data (n) in list
-* @head: pointer to the list head
-* Return: sum of data, or 0
+* sum_listint - Sum the data (n) in list.
+* @head: Pointer 2 the list head.
+* Return: Sum of data otherwise 0.
 **/
 int sum_listint(listint_t *head)
 {
