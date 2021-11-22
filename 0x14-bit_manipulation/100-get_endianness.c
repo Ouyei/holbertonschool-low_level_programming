97df7fd9f44ece2c2d0dc13687be5e500a89ce51
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-22 06:26:36 -0800

    README_updated, fixed_files

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -3,7 +3,7 @@
  * get_endianness - checks for endianness
  * Return: 0 if big endian or 1 if little
  */
-int get_endianness(void)
+int get_endianness(void);
 {
 	unsigned int i = 1;
 	char *c = (char *) &i;
