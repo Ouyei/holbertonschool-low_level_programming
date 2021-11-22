885d2cd3e4a285471e028f324b03617bce7f0ad6
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-22 07:13:00 -0800

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* read_textfile - Reads a text file and prints in POSIX stdout.
+* @filename: File 2 read.
+* @letters: Number of letter it should read and print.
+* Return: Actual number of letter it could read and print.
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+    int fdest, size;
+    char *buffer;
+    buffer = malloc(sizeof(char) * letters);
+
+    /*Check content file*/
+    if (!filename)
+    return (0);
+    /*Check value of buffer*/
+    if (!buffer)
+    return (0);
+    /*Open file*/
+    fdest = open(filename, O_RDONLY);
+    /*Read file*/
+    size = write(STDOUT_FILENO, buffer, read(fdest, buffer, letters));
+    if (fdest == -1 || size == -1)
+    return (0);
+    close(fdest);
+    free(buffer);
+    return (size);
+}
