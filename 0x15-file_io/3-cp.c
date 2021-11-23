a535d0f881ee588985cab2b6f92547b5e2a18909
Ouyei <36935397+Ouyei@users.noreply.github.com>
2021-11-23 08:00:51 -0800

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,8 +18,7 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd_in = open(argv[1], O_RDONLY);
 	if (fd_in == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode_file);
 	if (fd_out == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
@@ -28,8 +27,7 @@ int main(int argc, char **argv)
 	{
 		read_file = read(fd_in, buffer, BUFFERSIZE);
 		if (read_file == -1) /*If can't read*/
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 		if (read_file > 0)
 		{
 			check = write(fd_out, buffer, read_file);
