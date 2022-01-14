90bb94aa252b375f14f1a61a9b817c3648341726
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-14 10:47:31 -0800

    final_files

--- 0x18-dynamic_libraries/pruebas/source-0.c
+++
@@ -1,8 +0,0 @@
-/**
- * source_0 - Source 0
- * Return: Always 0
- */
-int source_0(void)
-{
-	return (0);
-}
--- 0x18-dynamic_libraries/pruebas/source-1.c
+++
@@ -1,8 +0,0 @@
-/**
- * source_1 - Source 1
- * Return: Always 0
- */
-int source_1(void)
-{
-	return (0);
-}
