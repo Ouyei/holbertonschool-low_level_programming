8e2d1e4ebab9ff825916fbf636df226f3fe85a85
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-27 06:25:37 -0800

    updated_files

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+  * hash_table_create - Creates a hash table.
+  * @size: Size of the array.
+  * Return: A pointer 2 the newly created hash table.
+  */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+	unsigned long int index;
+	/*Handle errors*/
+	if (size == 0)
+		return (NULL);
+
+	/*Allocated memory*/
+	new_table = malloc(sizeof(hash_table_t));
+	if (new_table == NULL)
+		return (NULL);
+
+	/*Allocated memory*/
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+		return (NULL);
+
+	new_table->size = size;
+
+	for (index = 0; index < size; index++)
+		new_table->array[index] = NULL;
+
+	return (new_table);
+}
