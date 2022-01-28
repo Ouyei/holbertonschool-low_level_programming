a1424ade1f3282f1adf788b9a69497bbbccfb1e1
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-28 13:46:06 -0800

    6-hash_table_delete.c

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+  * hash_table_delete - Deletes a hash table.
+  * @ht: The hash table to frees.
+  * Return: Nothing.
+  */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *elem, *next;
+	unsigned long int index;
+
+	if (ht == NULL)
+		return;
+	for (index = 0; index < ht->size; index++)
+	{
+		for (elem = ht->array[index];elem != NULL; elem = next)
+		{
+			next = elem->next;
+			free(elem->key);
+			free(elem->value);
+			free(elem);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
