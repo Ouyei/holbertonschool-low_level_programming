e51638715c32dfa5bfb95a047af0a93887777629
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-01-28 08:59:23 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,29 +10,29 @@
   */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned int long index;
-	hash_node_t *new_node, *elem;
-	char new_value;
+	unsigned long int index = 0;
+	hash_node_t *elem = NULL, *new_node = NULL;
 
 	if (ht == NULL || key == NULL || *key == 0 || value == NULL)
 		return (0);
 	/* Gives you the index of a key*/
 	index = key_index((unsigned char *) key, ht->size);
 	elem = ht->array[index];
-	
+
 	if (elem && strcmp(key, elem->key) == 0) /*Compare values*/
 	{
 		free(elem->value);
-		elem->value = stdrup(value);
+		elem->value = strdup(value);
 		return (1);
 	}
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (0); /*Handle errors*/
 
-	/*Handle errors*/
-
-	/*Copy key*/
-
-	/*Copy value*/
+	new_node->key = strdup(key); /*Copy key*/
+	new_node->value = strdup(value); /*Copy value*/
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
