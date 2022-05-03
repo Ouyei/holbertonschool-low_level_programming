b3acd0c677c2d19620d50a37923bee35f26ee9bf
Ouyei <obedat@.com>
2022-05-03 08:53:42 -0700

    task0_requirement

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -15,16 +15,12 @@ int linear_search(int *array, size_t size, int value)
 	if (array == NULL)
 		return (-1);
 
-	for (; i < size; ++i)
+	for (; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 
 		if (array[i] == value)
-			break;
+			return (i);
 	}
-
-	if (i == size)
-		return (-1);
-
-	return (i);
+	return (-1);
 }
