fa9d7480b49da9a0730731f17c832f6ec7aa0cdd
Ouyei <36935397+Ouyei@users.noreply.github.com>
2022-05-03 09:48:09 -0300

    Update 0-linear.c

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#ifndef __search_algos__
+#define __search_algos__
+#include <stdlib.h>
+#include <stdio.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* __search_algos__ */
